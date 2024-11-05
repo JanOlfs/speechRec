12d1bdff662151ed4a7b5eb72f9058d1a0cd343b
janolfs <j.olfs@amerdis.de>
2024-11-05 16:07:20 +0100

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'speech-rec',
+  webDir: 'dist/speech-rec',
+};
+
+export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { SpeechRecognition } from '@capacitor-community/speech-recognition';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'speechRec';
+  recording = false;
+  text = '';
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {
+    SpeechRecognition.requestPermissions()
+  }
+
+  async startListening() {
+    this.recording = true;
+    const { available } = await SpeechRecognition.available();
+    if(available) {
+      SpeechRecognition.start({
+        popup: false,
+        partialResults: true,
+        language: 'de-DE'
+      });
+      SpeechRecognition.addListener('partialResults', (data: any) => {
+        console.log("partialResults was fired", data.matches);
+        if(data.matches && data.matches.length > 0) {
+          this.text = data.matches[0];
+          this.changeDetectorRef.detectChanges();
+        }
+      });
+    }
+  }
+
+  async stopListening() {
+    this.recording = false;
+    await SpeechRecognition.stop()
+  }
+
 }
